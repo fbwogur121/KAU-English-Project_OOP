3e8625ecc14b0c787b3039ee3982f7453041759c
choi suyeon <csy5271abcd@gmail.com>
2024-11-10 19:02:15 +0900

    Local Changes Before Merging

--- app/src/main/java/com/example/kau_english_oop/MainActivity.kt
+++ app/src/main/java/com/example/kau_english_oop/MainActivity.kt
@@ -1,20 +1,71 @@
 package com.example.kau_english_oop
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.kau_english_oop.databinding.ActivityMainBinding
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // 회원가입 화면으로 이동하는 버튼 설정
-        findViewById<Button>(R.id.register_button).setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
+
+
+        // 초기 화면 설정
+        loadFragment(HomeFragment())
+
+        // BottomNavigationView 아이템 선택 리스너 설정
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_map -> {
+                    loadFragment(MapFragment())
+
+                    true
+                }
+                R.id.nav_camera -> {
+                    loadFragment(CameraFragment())
+
+                    true
+                }
+                R.id.nav_like -> {
+                    loadFragment(LikeFragment())
+
+                    true
+                }
+                R.id.nav_calendar -> {
+                    loadFragment(CalendarFragment())
+
+                    true
+                }
+                else -> false
+            }
         }
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, fragment)
+        transaction.commit()
+    }
+
 }
+
+
+
+
+
+
+
