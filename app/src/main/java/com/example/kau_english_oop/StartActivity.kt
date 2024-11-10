8727d2c1a94ef6515458fab722bacb62d167f1ca
joon041290 <joon041290@gmail.com>
2024-11-10 18:28:44 +0900

    StartActivity 수정

--- app/src/main/java/com/example/kau_english_oop/StartActivity.kt
+++ app/src/main/java/com/example/kau_english_oop/StartActivity.kt
@@ -3,15 +3,15 @@ package com.example.kau_english_oop
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.kau_english_oop.databinding.ActivityMainBinding
+import com.example.kau_english_oop.databinding.ActivityStartBinding
 
-class MainActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityStartBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // 회원가입 화면으로 이동하는 버튼 클릭 이벤트 처리
