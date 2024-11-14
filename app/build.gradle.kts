14afff707b9242e1b3d72ccf2095433e9217b370
fbwougr121 <fbwogur121@naver.com>
2024-11-14 13:35:33 +0900

    add menu button, exit button in conversation fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
     buildFeatures{
         viewBinding=true
+        dataBinding=true
     }
 }
 
@@ -62,10 +63,28 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     implementation(libs.firebase.database)
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.google.firebase:firebase-storage:21.0.1")
+
+
+    // Firebase BOM
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+
+    // Firebase Analytics 및 Authentication
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+
+    // Google Sign-In
+
+    implementation("com.google.android.gms:play-services-auth:20.5.0") // 최신 버전으로 변경
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor( "com.github.bumptech.glide:compiler:4.12.0")
 }
