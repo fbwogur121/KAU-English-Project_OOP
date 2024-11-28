65fa20f99434b9cd44da25d0a27fe87ce0eebbd4
fbwougr121 <fbwogur121@naver.com>
2024-11-28 15:20:12 +0900

    addapt MVVM pattern in HomePage

---
+++ app/src/main/java/com/example/kau_english_oop/ViewModel/HoemViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.kau_english_oop.ViewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.kau_english_oop.R
+
+class HomeViewModel : ViewModel() {
+    private val _imageButtonList = MutableLiveData<List<Int>>()
+    val imageButtonList: LiveData<List<Int>> get() = _imageButtonList
+
+    init {
+        loadImageButtonList()
+    }
+
+    private fun loadImageButtonList() {
+        _imageButtonList.value = listOf(
+            R.drawable.lexilearn,
+            R.drawable.journal_button,
+            R.drawable.youtube_button,
+            R.drawable.today_button,
+            R.drawable.shopping_button
+        )
+    }
+}
